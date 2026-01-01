5cec635d7459872553d50b5200ff3dc74aaac8cb
TeZLa369 <kuntalparashar046@gmail.com>
2026-01-01 18:34:00 +0530

    Fix formatting in AddExpenses styles for consistency

--- screens/AddExpenses.js
+++ screens/AddExpenses.js
@@ -277,7 +277,7 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         borderWidth: 1,
         borderColor: "rgba(255,255,255,0.05)",
-        justifyContent:"space-between"
+        justifyContent: "space-between"
 
     },
     memberAvatar: {
